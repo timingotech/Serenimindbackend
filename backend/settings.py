bd69ad752724eccb236e0d4d1b8eb71a65ec8a3b
Ridwan Oyenuga <taonuga01@gmail.com>
2024-03-30 11:08:08 +0100

    added to solve comunities issue

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-bot(2wug8-7lj_o5oo0!e#!i@xtzx8ypj=t(5uj^d=br4uxt^b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True        
 
-ALLOWED_HOSTS = ['.vercel.app','now.sh','timithemagician.pythonanywhere.com','localhost','.render.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
